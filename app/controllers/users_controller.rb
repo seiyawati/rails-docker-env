6ee729ddfb9036c3f1686a51293bde9870c4258d
Seiya Kawamoto <kid960805@gmail.com>
2021-01-19 18:23:51 +0900

    add: chapter seven by RSepc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      # 下と同義redirect_to user_url(@user)
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    # 適切に初期化したハッシュを返すparams[:user]のセキュリティ強化
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+        :password_confirmation)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 2021_01_18_162440) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.integer "price"
-    t.string "vendor"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
